c9a293a90c48aa01c0e811e321f65f3bf6910470
PRASANTH G <prasanth287471@gmail.com>
2021-03-23 16:19:38 +0000

    first commit

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
+import java.text.NumberFormat;
+
 class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
+NumberFormat currency=	NumberFormat.getCurrencyInstance();
+ String result=currency.format(123455643.4543);
+ System.out.println(result);
   }
 }
