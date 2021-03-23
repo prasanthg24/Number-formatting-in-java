782a1ad81db1e0d8ba4b719e3f08bd8136ea2410
PRASANTH G <prasanth287471@gmail.com>
2021-03-23 16:28:30 +0000

    second commit

--- Main.java
+++ Main.java
@@ -2,9 +2,19 @@ import java.text.NumberFormat;
 
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+System.out.println("Hello world!");
 NumberFormat currency=	NumberFormat.getCurrencyInstance();
  String result=currency.format(123455643.4543);
  System.out.println(result);
+
+NumberFormat percent=	NumberFormat.getPercentInstance();
+String result1=percent.format(0.1);
+System.out.println(result1);
+
+
+String result2=NumberFormat.getPercentInstance.format(0.2);
+System.out.println(result2);
+
+
   }
 }
